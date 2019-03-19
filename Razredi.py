be58af04bac2b2100911e60233e8936986a6e5b5
LMarkelj <redpandasagee@gmail.com>
2019-03-19 10:46:39 +0100

    initial commit

---
+++ Razredi.py
@@ -0,0 +1,27 @@
+class Predmet():
+    '''Hrani podatke o predmetu'''
+    def __init__(self,ime,vaje,predavanja,kreditneT):
+        '''pocisti kodo in zavaruj'''
+        self.ime=ime
+        self.vaje=vaje
+        self.predavanja=predavanja
+        self.kreditneT=kreditneT
+    
+class Program():
+    '''hrani podatke o programu'''
+    def __init__(self,ime,stopnja,predmeti):
+        '''Zavaruj'''
+        self.predmeti=predmeti
+        self.ime=ime
+        self.stopnja=stopnja
+
+class Fakulteta():
+    '''Hrani podatke o fakulteti'''
+    def __init__(self,ime,programi):
+        '''zavaruj'''
+        self.ime=ime
+        self.programi=programi
+
+
+
+
