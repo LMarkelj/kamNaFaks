71cd64f965a7226674c6532a7f4cf2d993f34872
eJereb <evajrb@gmail.com>
2019-03-29 10:39:45 +0100

    29.3.

--- luscenje.py
+++ luscenje.py
@@ -10,17 +10,15 @@ stran = requests.get('http://www.ung.si/sl/studij/fakulteta-za-naravoslovje/stud
 tabela = re.split(r'<table class="table table-hover">', stran)[2:]
 
 for i in range(len(tabela)):
-    tabela[i] = re.split(r'<tr>', tabela[i])[1:]
+    tabela[i] = re.split(r'<tr>', tabela[i])[1:] #pregleda tabele in splita po <td>
     for j in range(len(tabela[i])):
         tabela[i][j] = re.split(r'<td>', tabela[i][j])[1:]
         for k in range(len(tabela[i][j])):
-            tabela[i][j][k] = odstrani_html(tabela[i][j][k]).rstrip()
+            tabela[i][j][k] = re.split('\s', odstrani_html(tabela[i][j][k])) #odstrani vse ''white space''
+            
 
 
-
-
-
-
-
-    
+            
+           
+            
 print(tabela)
