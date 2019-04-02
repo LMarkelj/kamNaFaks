8679a6386504abe1d3cd1614b109182517e84f34
LMarkelj <redpandasagee@gmail.com>
2019-04-02 11:34:33 +0200

    Dodano zaceno iskanje

--- main.py
+++ main.py
@@ -1,33 +1,46 @@
 import Razredi
 import fizMar
-#import luscenje
+import luscenje
 
 def dobi_podatke():
     '''funkcija vrne seznam vseh faksov'''
     sezF = []
     sezF.append(fizMar.inciliziraj_mar())
+    #sezF.append(luscenje.preberi_stran())
 
     return sezF
 
 def izpisVsega(sezF):
     '''izpise vse fakse in programe'''
-    for element in sezF:
-        print(element)
+    for faks in sezF:
+        print(faks)
 
-def iskanjePoPredmetu(sezF,imePredmta):
+def iskanjePoPredmetu(sezF,imePredmeta):
     '''Funkcija izpise vse fakse z danim predmetom'''
-    
+    for faks in sezF:
+        zeNapisan = 0
+        for program in faks.programi:
+            for predmet in program.predmeti:
+                if predmet.ime == imePredmeta:
+                    print (faks.ime)
+                    zeNapisan = 1
+                    break
+            if zeNapisan == 1:
+                break
+
+
 
 
 
 
 def main():
     sezF = dobi_podatke()
-    izbira = int(input('1. Izpiši vse fakse ter njihove programe\n2. Najdi vse fakse ki imajo dani predmet\n3. Zapri program\n Izberite: '))
+    izbira = int(input('1. Izpiši vse fakse ter njihove programe\n2. Najdi vse fakse ki imajo dani predmet\n3. Zapri program\n\nIzberite: '))
     if izbira == 1:
         izpisVsega(sezF)
     elif izbira == 2:
-        print("V delu 2")
+        imePredmeta = input("Vpišite ime predmeta: ")
+        iskanjePoPredmetu(sezF,imePredmeta)
     elif izbira == 3:
         pass
 
