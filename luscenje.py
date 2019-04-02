146414e541a1ee2c29ae64b92790a6adf5024452
eJereb <evajrb@gmail.com>
2019-04-02 11:55:35 +0200

    skoraj final

--- luscenje.py
+++ luscenje.py
@@ -4,6 +4,7 @@ from Razredi import *
 
 def preberi_stran(stran_html, ime, stopnja):
     '''prebere podatke iz html-ja '''
+    nov_seznam = []
     stran = requests.get(stran_html).text 
     #prebere stran 
     tabela = re.split(r'<table class="table table-hover">', stran)[2:]
@@ -13,12 +14,15 @@ def preberi_stran(stran_html, ime, stopnja):
             tabela[i][j] = re.split(r'</td>', tabela[i][j])
             for k in range(len(tabela[i][j])):
                 tabela[i][j][k] = odstrani_html(tabela[i][j][k]).strip('\n').lstrip(' ').replace("\n", "").rstrip(" ").lstrip(" ") #odstrani vse ''white space''
-        
+            nov_seznam.append(Predmet(tabela[i][j][0], tabela[i][j][1], tabela[i][j][2]))
     tabela[-1][-1][-1] = 0 #namesto zadnjega dela html-ja zapiše 0
-    nov_seznam = []
-    nov_seznam.append(Predmet(tabela[i][j][0], tabela[i][j][1], tabela[i][j][2]))
     return Program(ime, stopnja, nov_seznam) 
 
-preberi_stran('http://www.ung.si/sl/studij/fakulteta-za-naravoslovje/studij/1FAF/', 'fizika NG', 'prva')
+
+preberi_stran('http://www.ung.si/sl/studij/fakulteta-za-naravoslovje/studij/1FAF/', 'fizika Nova Gorica', '1')
 
 
+def ime_fakultete():
+    programi=[preberi_stran('http://www.ung.si/sl/studij/fakulteta-za-naravoslovje/studij/1FAF/', 'fizika Nova Gorica', '1')]
+    ime = 'Fakulteta za naravoslovje'
+    return Fakulteta(ime, programi)
